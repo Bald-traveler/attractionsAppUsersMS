43ef0451deb3fb8c06e922b1d3bde6f2056b395b
heathdj <heathdj@gmail.com>
2021-12-02 14:18:36 -0500

    initial commit

---
+++ src/main/java/com/baldtraveler/api/attractionappuserms/AttractionAppUserMsApplication.java
@@ -0,0 +1,13 @@
+package com.baldtraveler.api.attractionappuserms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttractionAppUserMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttractionAppUserMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baldtraveler/api/attractionappuserms/AttractionAppUserMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baldtraveler.api.attractionappuserms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttractionAppUserMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
